36f536015b56eb548fede797bdec7d7e8cb6c426
Kay-Zee <kan@axiomzen.co>
2020-07-08 23:35:29 -0700

    A

--- container.go
+++ container.go
@@ -389,7 +389,7 @@ func (c *Container) remove() {
 		Force:         true,
 		RemoveVolumes: true,
 	}); err != nil {
-		printf(err.Error())
+		printf("(remove ) %-25s (%s) - err removing %s", c.Name, c.ID, err.Error())
 		c.t.Fatalf("container removal failure: %s", err.Error())
 	}
 	c.removed = true
